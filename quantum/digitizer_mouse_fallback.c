6031c509317623d8cff9c7da9a4ee51547c61a0a
miikamenk <miikarito@gmail.com>
2026-01-08 23:54:42 +0200

    match dilemma dpi

--- quantum/digitizer_mouse_fallback.c
+++ quantum/digitizer_mouse_fallback.c
@@ -61,7 +61,7 @@
 #    endif
 
 #    ifndef DIGITIZER_MAX_CPI
-#        define DIGITIZER_MAX_CPI 3200
+#        define DIGITIZER_MAX_CPI 3400
 #    endif
 
 #    ifdef DIGITIZER_REPORT_TAPS_AS_CLICKS
